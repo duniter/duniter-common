a85b7268fda5d22288ac3342407f091871d516bb
cgeek <cem.moreau@gmail.com>
2017-01-18 19:09:54 +0100

    Initialization

---
+++ README.rst
@@ -0,0 +1,11 @@
+duniter-common
+==============
+
+An NPM module providing the common tools used for Duniter_ development (both core and modules).
+
+Tools
+-----
+
+* `hashf`: the hash function largely used in Duniter (currently an alias to SHA256)
+
+.. _Duniter: https://github.com/duniter/duniter
---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  hashf: require('./lib/hashf'),
+  dos2unix: require('./lib/dos2unix'),
+  rules: require('./lib/rules'),
+  keyring: require('./lib/crypto/keyring'),
+  base58: require('./lib/crypto/base58'),
+  rawer: require('./lib/rawer'),
+  buid: require('./lib/buid')
+}
