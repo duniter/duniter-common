cfcf096fd4a23ba222e043ebc3c5849964711820
cgeek <cem.moreau@gmail.com>
2017-06-28 11:59:41 +0200

    [enh] duniter/duniter#875 Migrate back 'indexer' and 'rules' to duniter core

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 module.exports = {
   hashf: require('./lib/hashf'),
   dos2unix: require('./lib/dos2unix'),
-  rules: require('./lib/rules'),
   keyring: require('./lib/crypto/keyring'),
   base58: require('./lib/crypto/base58'),
   rawer: require('./lib/rawer'),
@@ -11,6 +10,5 @@ module.exports = {
   document: require('./lib/document'),
   constants: require('./lib/constants'),
   txunlock: require('./lib/txunlock'),
-  indexer: require('./lib/indexer'),
   parsers: require('./lib/parsers')
 }
